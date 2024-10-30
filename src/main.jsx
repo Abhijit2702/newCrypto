625c70af94b123afd81d0992d975efcd075350fd
abhijitrehpade8 <abhijitrehpade8@gmail.com>
2024-10-30 11:06:30 +0530

    main

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { CryptoProvider } from "./cryptoContext";
+import { CryptoProvider } from "./CryptoContext";
 import "react-alice-carousel/lib/alice-carousel.css";
 
 createRoot(document.getElementById("root")).render(
